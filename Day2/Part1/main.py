7047c54a603ac86f32407fb41e4ed1171246ec80
linozadravec <lzadravec@foi.hr>
2025-12-02 21:21:09 +0100

    Day2

--- Day2/Part1/main.py
+++ Day2/Part1/main.py
@@ -1,6 +1,23 @@
 def getInput(filename):
-    ...
+    file = open(filename)
+    input = file.readlines()
 
+    input = input[0].split(",")
+    input = [i.strip() for i in input]
+    
+    return input
+
+def processInput(input):
+    sum = 0
+    for e in input:
+        #print(e)
+        first = int(e.split("-")[0])
+        last = int(e.split("-")[1])
+        for num in range(first, last+1):
+            if len(str(num)) % 2 == 0 and num // pow(10,len(str(num))/2) == num % pow(10,len(str(num))/2):
+                print(num)
+                sum += num
+    print("RESULT: " + str(sum))
 
 def getData(input):
     ...
@@ -10,12 +27,8 @@ def getResult(data):
     ...
 
 
+input = getInput("Repos/Day2/Part1/input.txt")
+
+processInput(input)
 
 
-
-input = getInput("input.txt")
-data = getData(input)
-result = getResult(data)
-print(result)
-resultFile = open("result.txt", "w")
-resultFile.write(str(result))
--- Day2/Part2/main.py
+++ Day2/Part2/main.py
@@ -1,6 +1,28 @@
 def getInput(filename):
-    ...
+    file = open(filename)
+    input = file.readlines()
 
+    input = input[0].split(",")
+    input = [i.strip() for i in input]
+    
+    return input
+
+def processInput(input):
+    sum = 0
+    for e in input:
+        first = int(e.split("-")[0])
+        last = int(e.split("-")[1])
+        for num in range(first, last+1):
+            for length in range(1, int((len(str(num))/2)+1)):
+                if len(str(num)) % length == 0:
+                    part_str = str(num)[:length]
+                    if part_str * int((len(str(num)) / length)) == str(num):
+                        print(num)
+                        sum += num
+                        break
+
+                
+    print("RESULT: " + str(sum))
 
 def getData(input):
     ...
@@ -12,10 +34,8 @@ def getResult(data):
 
 
 
+input = getInput("Repos/Day2/Part2/input.txt")
 
-input = getInput("input.txt")
-data = getData(input)
-result = getResult(data)
-print(result)
-resultFile = open("result.txt", "w")
-resultFile.write(str(result))
+processInput(input)
+
+
