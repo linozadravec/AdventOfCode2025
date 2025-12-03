2b40fd0ff468ec997e29eafa560dc57b1b86dc31
linozadravec <lzadravec@foi.hr>
2025-12-03 23:01:54 +0100

    Day3

--- Day3/Part1/main.py
+++ Day3/Part1/main.py
@@ -1,5 +1,23 @@
 def getInput(filename):
-    ...
+    file = open(filename)
+    input = file.readlines()
+    input = [i.strip() for i in input]
+    return input
+
+def processInput(input):
+    sum = 0
+    for line in input:
+        max_i = 0
+        for i in range(1,len(line)-1):
+            if line[max_i] < line[i]:
+                max_i = i
+
+        max_j = max_i + 1
+        for j in range(max_j, len(line)):
+            if line[max_j] < line[j]: 
+                max_j = j
+        sum += int(str(line[max_i]) + str(line[max_j]))
+    print(sum)
 
 
 def getData(input):
@@ -10,12 +28,16 @@ def getResult(data):
     ...
 
 
+input = getInput("Repos/Day3/Part1/input.txt")
+
+processInput(input)
 
 
 
-input = getInput("input.txt")
-data = getData(input)
-result = getResult(data)
-print(result)
-resultFile = open("result.txt", "w")
-resultFile.write(str(result))
+#data = getData(input)
+#result = getResult(data)
+#print(input)
+#resultFile = open("result.txt", "w")
+#resultFile.write(str(result))
+
+
--- Day3/Part2/main.py
+++ Day3/Part2/main.py
@@ -1,5 +1,36 @@
 def getInput(filename):
-    ...
+    file = open(filename)
+    input = file.readlines()
+    input = [i.strip() for i in input]
+    return input
+
+def processInput(input):
+    sum = 0
+    for line in input:
+        result = ""
+        # SOLUTION W/OUT max_i
+        current = 0
+        for remaining in range(11, -1, -1):
+            for i in range(current, len(line) - remaining):
+                if line[current] < line[i]:
+                    current = i
+            result += line[current]
+            current += 1
+        sum += int(result)
+    
+
+        # SOLUTION
+        # current = 1
+        # max_i = 0
+        # for remaining in range(12, 0, -1):
+        #     for i in range(current,len(line)-remaining + 1):
+        #         if line[max_i] < line[i]:
+        #             max_i = i
+        #     result += line[max_i]
+        #     max_i = max_i + 1
+        #     current = max_i + 1
+        # sum += int(result)
+    print(sum)
 
 
 def getData(input):
@@ -10,12 +41,16 @@ def getResult(data):
     ...
 
 
+input = getInput("Repos/Day3/Part2/input.txt")
+
+processInput(input)
 
 
 
-input = getInput("input.txt")
-data = getData(input)
-result = getResult(data)
-print(result)
-resultFile = open("result.txt", "w")
-resultFile.write(str(result))
+#data = getData(input)
+#result = getResult(data)
+#print(input)
+#resultFile = open("result.txt", "w")
+#resultFile.write(str(result))
+
+
