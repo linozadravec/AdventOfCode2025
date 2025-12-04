d42a4b7112cc624554f6726b9845f02428cb16f8
linozadravec <lzadravec@foi.hr>
2025-12-04 23:05:41 +0100

    Day4

--- Day4/Part1/main.py
+++ Day4/Part1/main.py
@@ -1,5 +1,53 @@
 def getInput(filename):
-    ...
+    file = open(filename)
+    input = file.readlines()
+    input = [i.strip() for i in input]
+    return input
+
+def processInput(input):
+    sum = 0
+    rows = len(input)
+    columns = len(input[0])
+    # arr_adjecency = [[0] * columns] * rows # WRONG, references same object
+    arr_adjecency = [[0 for x in range(columns)] for y in range(rows)] 
+    
+
+    # arr_adjecency[1][1] = 1
+
+    for r in range(0,rows):
+        lin = ""
+        for c in range(0, columns):
+            lin += str(arr_adjecency[r][c]) + " "
+        print(lin)
+    
+    arr_nearby  = [[-1,-1], [-1,0], [-1,1], [0,-1], [0,1], [1,-1], [1,0], [1,1]]
+
+    for idx_line, line in enumerate(input):
+        for idx_char, char in enumerate(line):
+            #print(str(idx_line) + " " + str(idx_char))
+            if char == "@":
+                #print("FOUND @ on (" + str(idx_line) + ", " + str(idx_char) + ")")
+                for nearby in arr_nearby:
+                    if 0 <= idx_line + nearby[0] < rows and  0 <= idx_char + nearby[1] < columns:
+                        #print(str(idx_line + nearby[0]) + " " + str(idx_char + nearby[1]))
+                        # print()
+                        arr_adjecency[idx_line + nearby[0]][idx_char + nearby[1]] += 1
+                        # arr_adjecency[idx_char + nearby[0]][idx_line + nearby[1]] += 1
+
+                # print(line.index)
+                # print(char.index)
+    for idx_line, line in enumerate(input):
+        for idx_char, char in enumerate(line):
+            if char == "@" and arr_adjecency[idx_line][idx_char] < 4:
+                sum += 1
+    # lin = 
+    # for r in range(0,rows):
+    #     lin = ""
+    #     for c in range(0, columns):
+    #         lin += str(arr_adjecency[r][c]) + " "
+    #     print(lin)
+    
+    print(sum)
 
 
 def getData(input):
@@ -10,12 +58,16 @@ def getResult(data):
     ...
 
 
+input = getInput("Repos/Day4/Part1/input.txt")
+
+processInput(input)
 
 
 
-input = getInput("input.txt")
-data = getData(input)
-result = getResult(data)
-print(result)
-resultFile = open("result.txt", "w")
-resultFile.write(str(result))
+#data = getData(input)
+#result = getResult(data)
+#print(input)
+#resultFile = open("result.txt", "w")
+#resultFile.write(str(result))
+
+
--- Day4/Part2/main.py
+++ Day4/Part2/main.py
@@ -1,5 +1,84 @@
 def getInput(filename):
-    ...
+    file = open(filename)
+    input = file.readlines()
+    input = [i.strip() for i in input]
+    return input
+
+def processInput(input):
+    sum = 0
+    rows = len(input)
+    columns = len(input[0])
+    # arr_adjecency = [[0] * columns] * rows # WRONG, references same object
+    arr_adjecency = [[0 for x in range(columns)] for y in range(rows)] 
+    
+
+    # arr_adjecency[1][1] = 1
+
+    for r in range(0,rows):
+        lin = ""
+        for c in range(0, columns):
+            lin += str(arr_adjecency[r][c]) + " "
+        print(lin)
+    
+    arr_nearby  = [[-1,-1], [-1,0], [-1,1], [0,-1], [0,1], [1,-1], [1,0], [1,1]]
+
+    for idx_line, line in enumerate(input):
+        for idx_char, char in enumerate(line):
+            #print(str(idx_line) + " " + str(idx_char))
+            if char == "@":
+                #print("FOUND @ on (" + str(idx_line) + ", " + str(idx_char) + ")")
+                for nearby in arr_nearby:
+                    if 0 <= idx_line + nearby[0] < rows and  0 <= idx_char + nearby[1] < columns:
+                        #print(str(idx_line + nearby[0]) + " " + str(idx_char + nearby[1]))
+                        # print()
+                        arr_adjecency[idx_line + nearby[0]][idx_char + nearby[1]] += 1
+                        # arr_adjecency[idx_char + nearby[0]][idx_line + nearby[1]] += 1
+
+                # print(line.index)
+                # print(char.index)
+    while(True):
+        count = 0
+        print(count)
+        for idx_line, line in enumerate(input):
+            chars = list(line)
+            for idx_char, char in enumerate(line):
+                if char == "@" and arr_adjecency[idx_line][idx_char] < 4:
+                    chars[idx_char] = "x" 
+                    count += 1
+                    sum += 1 # sum removed
+                    for nearby in arr_nearby:
+                        if 0 <= idx_line + nearby[0] < rows and  0 <= idx_char + nearby[1] < columns:
+                            #print(str(idx_line + nearby[0]) + " " + str(idx_char + nearby[1]))
+                            # print()
+                            if arr_adjecency[idx_line + nearby[0]][idx_char + nearby[1]] > 0:
+                                arr_adjecency[idx_line + nearby[0]][idx_char + nearby[1]] -= 1
+            input[idx_line] = chars
+
+        # for r in range(0, rows):
+        #     lin = ""
+        #     for c in range(0, columns):
+        #         lin += str(input[r][c]) + " "
+        #     print(lin)
+
+        # for r in range(0,rows):
+        #     lin = ""
+        #     for c in range(0, columns):
+        #         lin += str(arr_adjecency[r][c]) + " "
+        #     print(lin)
+        
+        print(count)
+        if count == 0:
+            break
+
+    print(sum)                                
+                
+    # lin = 
+    # for r in range(0,rows):
+    #     lin = ""
+    #     for c in range(0, columns):
+    #         lin += str(arr_adjecency[r][c]) + " "
+    #     print(lin)
+    
 
 
 def getData(input):
@@ -10,12 +89,16 @@ def getResult(data):
     ...
 
 
+input = getInput("Repos/Day4/Part2/input.txt")
+
+processInput(input)
 
 
 
-input = getInput("input.txt")
-data = getData(input)
-result = getResult(data)
-print(result)
-resultFile = open("result.txt", "w")
-resultFile.write(str(result))
+#data = getData(input)
+#result = getResult(data)
+#print(input)
+#resultFile = open("result.txt", "w")
+#resultFile.write(str(result))
+
+
